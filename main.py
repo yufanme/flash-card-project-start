88b6dfd243ca3a9d6379e7887b2c3664b3097db5
Fan Yu <562937707@qq.com>
2022-01-12 23:33:02 +0800

    almost finish

--- main.py
+++ main.py
@@ -2,25 +2,36 @@ from tkinter import *
 import pandas
 import random
 
+current_card = {}
+
 # get df_data
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
 
 
+# ---------------------------------- remove word ----------------------------------- #
+def is_known():
+    to_learn.remove(current_card)
+    df = pandas.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv")
+
+
 # ---------------------------------- get random word ------------------------------- #
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(canvas_img, image=front_image_path)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
-    window.after(3000, change_card, current_card)
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, flip_card)
 
 
 # ---------------------------------- change to English card ------------------------ #
-def change_card(current_card):
-    canvas.itemconfig(canvas_img, image=back_image_path)
+def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -30,13 +41,15 @@ window = Tk()
 window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+flip_timer = window.after(3000, func=flip_card)
+
 # create canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 
 # image in canvas
-front_image_path = PhotoImage(file="./images/card_front.png")
-back_image_path = PhotoImage(file="images/card_back.png")
-canvas_img = canvas.create_image(400, 263, image=front_image_path)
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 
 
 # text in canvas
@@ -46,12 +59,12 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 # create buttons
 check_img = PhotoImage(file="images/right.png")
-known_button = Button(image=check_img, highlightthickness=0, command=next_card)
-known_button.grid(row=1, column=0)
+known_button = Button(image=check_img, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
 
 cross_img = PhotoImage(file="images/wrong.png")
 unknown_button = Button(image=cross_img, highlightthickness=0, command=next_card)
-unknown_button.grid(row=1, column=1)
+unknown_button.grid(row=1, column=0)
 
 next_card()
 
